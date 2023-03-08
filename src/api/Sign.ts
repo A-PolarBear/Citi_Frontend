26557782fbc2eae60acdd0d564e77d3e78a09e1b
A-Polar-Bear <11335885+a-polar-bear66@user.noreply.gitee.com>
2023-03-08 17:25:06 +0800

    figure out cors

--- src/api/Sign.ts
+++ src/api/Sign.ts
@@ -3,11 +3,11 @@ import instance from "./index"
 
 const LoginAPI = {
     login: (userInfo: any) =>
-        instance.get("/users/login",{params:userInfo}),
+        instance.get("/users/login",{params:userInfo,withCredentials:false}),
     signUp: (userInfo: any) =>
-        instance.post("/users", userInfo ),
+        instance.post("/users", {userInfo,withCredentials:false} ),
     forgetPwd: (userInfo: any) =>
-        instance.post("/users/forgetPwd", userInfo),
+        instance.post("/users/forgetPwd", {userInfo,withCredentials:false}),
 }
 
 export default LoginAPI;
