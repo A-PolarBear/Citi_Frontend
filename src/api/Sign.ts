aaddb1201af56c4f16c5f792ca8e698c4b597803
A-Polar-Bear <11335885+a-polar-bear66@user.noreply.gitee.com>
2023-03-11 23:29:37 +0800

    fix: pwd change

--- src/api/Sign.ts
+++ src/api/Sign.ts
@@ -6,7 +6,7 @@ const LoginAPI = {
   signUp: (userInfo: any) =>
     instance.post("/users/reg", userInfo),
   changePwd: (userInfo: any) =>
-    instance.put("/users/change-pwd", userInfo),
+    instance.put("/users/change-pwd", {params:userInfo}),
 };
 
 export default LoginAPI;
